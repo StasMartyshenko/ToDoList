1eaf83d08502a64836e3ecd541334076ca7252fe
sMarty <martyshenkostas@gmail.com>
2017-03-11 18:47:32 +0200

    11.03

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function todoList () {
-	var item = document.getElementById("toDoInput").value
-	var text = document.createTextNode(item)
-	var newItem = document.createElement("li")
-	newItem.appendChild(text)
-	document.getElementById("todoList").appendChild(newItem)
+	var item = document.getElementById("toDoInput").value;
+	var text = document.createTextNode(item);
+	var newItem = document.createElement("li");
+	newItem.appendChild(text);
+	document.getElementById("todoList").appendChild(newItem);
+    document.getElementById("toDoInput").value="";
 }
